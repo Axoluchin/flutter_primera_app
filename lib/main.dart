6c79e73dcef35e280ba0a3bbd7d74f60e415f59f
Damian Huchin <huchin.h.damian@gmail.com>
2023-06-18 00:06:12 -0600

    feat: Better UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,11 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Mi primera app by Axoluchin',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 139, 165, 248)),
         ),
         home: MyHomePage(),
       ),
@@ -27,20 +28,92 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var liked = false;
+
+  void newWorld() {
+    current = WordPair.random();
+    liked = false;
+    notifyListeners();
+  }
+
+  void setLiked() {
+    liked = true;
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var wordPair = appState.current;
 
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
+          Center(child: CartPair(wordPair: wordPair, appState: appState)),
         ],
       ),
+    );
+  }
+}
+
+class CartPair extends StatelessWidget {
+  const CartPair({
+    super.key,
+    required this.wordPair,
+    required this.appState,
+  });
+
+  final WordPair wordPair;
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    IconData icon;
+
+    if (appState.liked) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_outline;
+    }
+
+    return Card(
+      margin: EdgeInsets.all(16),
+      child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text(
+                wordPair.asPascalCase,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              Divider(),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FilledButton(
+                        onPressed: () {
+                          appState.newWorld();
+                        },
+                        child: Text('Cambiar palabra')),
+                    SizedBox(width: 32),
+                    FilledButton.tonalIcon(
+                      onPressed: () {
+                        appState.setLiked();
+                      },
+                      icon: Icon(icon, color: Colors.blue),
+                      label: Text('Like'),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
